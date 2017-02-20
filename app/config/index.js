8578d5452cf3073e87c54a59ba83b1090c8596d3
mauesrog <Mauricio.Esquivel.Rogel.18@Dartmouth.edu>
2017-02-20 12:09:30 -0800

    drive 3

--- app/config/index.js
+++ app/config/index.js
@@ -3,6 +3,17 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default {
-  secret: process.env.API_SECRET,
-  sendgridSecret: process.env.SENDGRID_API_KEY,
+  sendgrid: {
+    secret: process.env.API_SECRET,
+    sendgridSecret: process.env.SENDGRID_API_KEY,
+  },
+  google: {
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    clientId: process.env.GOOGLE_CLIENT_ID,
+    redirectUrl: process.env.GOOGLE_REDIRECT_URL,
+    accessToken: process.env.GOOGLE_ACCESS_TOKEN,
+    refreshToken: process.env.GOOGLE_REFRESH_TOKEN,
+    tokenType: process.env.GOOGLE_TOKEN_TYPE,
+    expiryDate: parseInt(process.env.GOOGLE_EXPIRY_DATE, 10),
+  },
 };
