2d35c18ad95e35e6f6fced56f4e50c78bb976bc1
mauesrog <Mauricio.Esquivel.Rogel.18@Dartmouth.edu>
2017-02-20 12:31:01 -0800

    fixed merge conflicts

--- app/router.js
+++ app/router.js
@@ -1,11 +1,7 @@
 import { Router } from 'express';
 import * as Projects from './controllers/project_controller';
 import * as Email from './controllers/email_controller';
-<<<<<<< HEAD
 import * as Drive from './controllers/drive_controller';
-=======
->>>>>>> efede5aaf8ac8558b0338396b47de4f6454dc51b
-
 
 const router = Router();
 
@@ -15,11 +11,8 @@ router.route('/projects')
 router.route('/contact')
       .post(Email.sendEmail);
 
-<<<<<<< HEAD
 router.route('/drive')
       .get(Drive.getResumeUrl)
       .put(Drive.updateResume);
 
-=======
->>>>>>> efede5aaf8ac8558b0338396b47de4f6454dc51b
 export default router;
